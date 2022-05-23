f56405a79556e011bacb14f5540ec82bce91f32d
Juan Buitrago <juanbuitrago2002@outlook.com>
2022-05-23 16:28:59 -0500

    bug fix

--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 function useLocalStorage(itemName, initialValue) {
-  
+
   const [error, setError] = React.useState(false);
   const [loading, setLoading] = React.useState(true);
   const [item, setItem] = React.useState(initialValue);
   const [sincronizedItem, setSincronizedItem] = React.useState(true)
 
-  
+
   React.useEffect(() => {
     setTimeout(() => {
       try {
         const localStorageItem = localStorage.getItem(itemName);
         let parsedItem;
-        
+
         if (!localStorageItem) {
           localStorage.setItem(itemName, JSON.stringify(initialValue));
           parsedItem = initialValue;
@@ -24,18 +24,19 @@ function useLocalStorage(itemName, initialValue) {
         setItem(parsedItem);
         setLoading(false);
         setSincronizedItem(true)
-      } catch(error) {
+      } catch (error) {
         setError(error);
       }
     }, 2000);
-      },[sincronizedItem]);
-  
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sincronizedItem]);
+
   const saveItem = (newItem) => {
     try {
       const stringifiedItem = JSON.stringify(newItem);
       localStorage.setItem(itemName, stringifiedItem);
       setItem(newItem);
-    } catch(error) {
+    } catch (error) {
       setError(error);
     }
   };
